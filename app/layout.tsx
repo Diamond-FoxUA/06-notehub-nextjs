afa8cc1551f617652d1027792c1e39ac7d21da17
Dmytro Farbun <dimofarb@gmail.com>
2025-11-29 21:20:16 +0200

    feat(api): add TanStackProvider component for React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import TanStackProvider from "./components/TanStackProvider/TanStackProvider";
 
 export const metadata: Metadata = {
   title: "NoteHub",
@@ -17,9 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header />
-        <main>{children}</main>
-        <Footer />
+        <TanStackProvider>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </TanStackProvider>
       </body>
     </html>
   )
