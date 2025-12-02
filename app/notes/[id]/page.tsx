45c335a0f7d00bbc926c313ea05dfbf723679bc0
Dmytro Farbun <dimofarb@gmail.com>
2025-12-02 17:57:41 +0200

    fix(note-details): destructure id from params to match fetchNoteById type

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -6,8 +6,8 @@ import {
 import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from './NoteDetails.client';
 
-export default async function NoteDetails({ params }: { params: Promise<{id: string}> }) {
-  const id = await params;
+export default async function NoteDetails({ params }: { params: Promise<{ id: string }>}) {
+  const { id } = await params;
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
